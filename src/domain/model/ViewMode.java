4d531eed56c546d9d5d4ff63cf8ef239b6cdfe24
Laurens Druwel <laurensdr@outlook.com>
2019-02-25 22:43:10 +0100

    viewmodes added as enum

---
+++ src/domain/model/ViewMode.java
@@ -0,0 +1,14 @@
+package domain.model;
+
+public enum ViewMode {
+	/**
+	 * An enumeration of View Modes.
+	 * 
+	 * @version 1.0
+	 * @author Laurens Druwel
+	 */
+	TABLEMODE,
+	TABLEDESIGNMODE,
+	TABLEROWMODE
+	
+}
