ef27a376417276a9bf14465f890c1356386b87a5
Mauro Luyten <mauro.luyten@gmail.com>
2019-03-06 19:44:00 +0100

    small fix

--- src/ui/model/viewmodes/TableRowsViewMode.java
+++ src/ui/model/viewmodes/TableRowsViewMode.java
@@ -12,8 +12,8 @@ import ui.model.view.View;
 public class TableRowsViewMode extends ViewMode {
 	private Container container;
 
-	public TableRowsViewMode(String name, View view) {
-		super(name, view);
+	public TableRowsViewMode(String name) {
+		super(name);
 		container = new Container(0, 0, 600, 500);
 		addDummyTable();
 		this.addComponent(container);
