9e7ad7b4e4f236ccb87a3ffc42305ab58ab27cd6
Dries Janse <djse@live.be>
2019-03-14 21:18:05 +0100

    ChangeHandlerInterface Doc

--- src/controller/handlers/ChangeHandlerInterface.java
+++ src/controller/handlers/ChangeHandlerInterface.java
@@ -5,8 +5,25 @@ import java.beans.PropertyChangeEvent;
 import domain.model.DomainFacadeInterface;
 import ui.model.view.UIFacadeInterface;
 
+/**
+ * An interface of the ChangeHandlerInterface.
+ *  Every changeHandler has to implement a handle method.
+ *  
+ * @version 1.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel, Mauro Luyten
+ *
+ */
 public interface ChangeHandlerInterface {
 
+	/**
+	 * 
+	 * @param evt
+	 *        | The propertyChangeEvent containing all the information of the event.
+	 * @param uiFacade
+	 *        | The uiFacadeInterface used.
+	 * @param domainFacade
+	 *        | The domainFacadeInterface used.
+	 */
 	public void handleChange(PropertyChangeEvent evt, UIFacadeInterface uifacade, DomainFacadeInterface domainfacade );
 	
 	
