3940ca234733abb2ea274843827149e5b92d2e96
Dries Janse <djse@live.be>
2019-03-14 20:03:42 +0100

    ValueType doc

--- src/domain/model/ValueType.java
+++ src/domain/model/ValueType.java
@@ -4,11 +4,12 @@ package domain.model;
  * An enumeration introducing the different types a Column can have.
  *   In its current form, the class only supports, Email, String, Boolean and Integer.
  * 
- * @version 1.0
- * @author Dries Janse
+ * @version 2.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel, Mauro Luyten
  */
 public enum ValueType {
 
+	
 	EMAIL("email@", String.class, "Email"), STRING("", String.class, "String"),
 	BOOLEAN(new Boolean(true), Boolean.class, "Boolean"), INTEGER(new Integer(0), Integer.class, "Integer");
 
@@ -22,18 +23,24 @@ public enum ValueType {
 	 */
 	private Class<?> cl;
 
+	/**
+	 * Variable storing the value String which can be displayed.
+	 */
 	private String displayValue;
 
 	/**
-	 * Initialise the type with the given default value and class object.
+	 * Initialise the type with the given default value, class object and displayValue.
 	 *  
 	 * @param defaultValue
 	 *        The default value of the type.
 	 * @param cl
 	 *        The default class object of the type.
-	 * @post the defaultValue and class Object are set with the given values.
-	 *        | new.getTypeClass() == cl
-	 *        | new.getDefaultValue() == defaultValue
+	 * @param displayValue
+	 *        The value used to display.
+	 * @effect the defaultValue, class Object and displayValue are set with the given values.
+	 *        | setDefaultValue(defaultValue)
+	 *        | setDisplayValue(displayValue)
+	 *        | setCl(cl)
 	 */
 	private ValueType(Object defaultValue, Class<?> cl, String displayValue) {
 		setDefaultValue(defaultValue);
@@ -41,21 +48,34 @@ public enum ValueType {
 		setCl(cl);
 	}
 
+	/**
+	 * Sets the class used for the valueType.
+	 * @param cl
+	 *        | The Class used for the value.
+	 * @post The class is set with the given class.
+	 *        | this.getTypeClass() == cl
+	 */
 	private void setCl(Class<?> cl) {
 		this.cl = cl;
 	}
 
+	/**
+	 * Sets the defaultValue of the valueType.
+	 * @param defaultValue
+	 *        | The default value of the valueType.
+	 * @post The default value is set for the valueType
+	 *       | this.getDefaultValue() == defaultValue
+	 */
 	private void setDefaultValue(Object defaultValue) {
 		this.defaultValue = defaultValue;
 	}
 
 	/**
-	 * Checks whether the given value is of the right type.
+	 * Checks whether value can be of the used in the valueType.
 	 * 
 	 * @param value
 	 *        The value to be checked.
-	 * @return True if the given value is valid; false otherwise
-	 *         | result == (value != null) && (value.getClass().equals( this.getTypeClass()))
+	 * @return True if the given value can be used with a valueType.
 	 */
 	public boolean canHaveAsValue(Object value) {
 		if (value == null) {
@@ -93,6 +113,12 @@ public enum ValueType {
 		}
 	}
 
+	/**
+	 * Returns the valueType when a display value is entered as parameter.
+	 * Null if parameter does not equal a default value.
+	 * @param name
+	 * @return the ValueType when name equals a displayValue of one of the valueTypes; otherwise null;
+	 */
 	public static ValueType getValueTypeForString(String name) {
 		for (ValueType type : ValueType.values()) {
 			if (type.getDisplayValue().equals(name))
@@ -115,19 +141,38 @@ public enum ValueType {
 		return this.defaultValue;
 	}
 
+	/**
+	 * Returns the display value of the valueType.
+	 */
 	@Override
 	public String toString() {
 		return displayValue;
 	}
 
+	/**
+	 * Returns the display value of the valueType.
+	 */
 	private String getDisplayValue() {
 		return displayValue;
 	}
 
+	/**
+	 * Sets the displayValue of the valueType.
+	 * @param displayValue
+	 *        | the display value of the type. The printable String of a type.
+	 */
 	private void setDisplayValue(String displayValue) {
 		this.displayValue = displayValue;
 	}
 
+	/**
+	 * Casts the value to ValueType.
+	 * If the value parameter equals null, null is returned.
+	 * @param value
+	 *        | The value which will be returned.
+	 * @return The casted value to the given ValueType.
+	 * 
+	 */
 	public Object castTo(Object value) {
 		if (value == null) {
 			return null;
