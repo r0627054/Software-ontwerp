ac8260fb1f13ab841b30b5b1fb3aa7ddc774ac4d
Dries Janse <djse@live.be>
2019-03-14 16:22:41 +0100

    App docs

--- src/app/App.java
+++ src/app/App.java
@@ -6,8 +6,24 @@ import domain.model.DomainFacadeInterface;
 import ui.model.view.UIFacade;
 import ui.model.view.UIFacadeInterface;
 
+/**
+ *  The Application class is used to start the Program.
+ *  It create an instance of the domainFacadeInterface and an instance of the UIFacadeInterface,
+ *  and gives it to the controller.
+ * @version 1.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel, Mauro Luyten
+ *
+ */
 public class App {
 
+	/**
+	 * It creates a DomainFacade Instance and a UIFacade Instance.
+	 * And it creates a Controller which links both the domain with the UI.
+	 * 
+	 * @param args
+	 *        | the arguments are not used.
+	 *        | the application starts without any arguments.
+	 */
 	public static void main(String[] args) {
 		java.awt.EventQueue.invokeLater(() -> {	
 			DomainFacadeInterface domainFacadeInstance = DomainFacade.getInstance();
