e825ca28bcc9df437adc29a4ba00679439af4c4d
Dries Janse <djse@live.be>
2019-03-15 16:49:31 +0100

    ViewMode doc

--- src/ui/model/viewmodes/ViewMode.java
+++ src/ui/model/viewmodes/ViewMode.java
@@ -12,31 +12,84 @@ import java.util.UUID;
 
 import ui.model.components.Component;
 
+/**
+ * A viewMode is a mode the view can have, it's an abstract class.
+ *  A viewMode is the actual view inside one frame.
+ *  The viewMode keeps track of all the components and all the clickListeners/KeyListeners of the components.
+ * 
+ * @version 1.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel, Mauro Luyten
+ *
+ */
 public abstract class ViewMode implements PropertyChangeListener {
+	
+	/**
+	 * The variable storing all the components of the specific viewMode.
+	 */
 	private List<Component> components = new ArrayList<>();
+	
+	/**
+	 * The variable storing all the clickListeners of the components.
+	 */
 	private List<Component> clickListeners = new ArrayList<>();
+	
+	/**
+	 * The variable storing all the keyListeners of the components.
+	 */
 	private List<Component> keyListeners = new ArrayList<>();
 
+	/**
+	 * The variable storing the propertyChangeSupport.
+	 */
 	private PropertyChangeSupport support;
 
+	/**
+	 * The variable storing the type of the viewMode.
+	 */
 	private ViewModeType type;
 
+	/**
+	 * Initialises a new ViewMode and sets the support variable.
+	 */
 	public ViewMode() {
 		support = new PropertyChangeSupport(this);
 	}
 
+	/**
+	 * Adds a propertyChangeListener to support variable.
+	 * @param pcl
+	 *        | The propertyChangeListener which needs to be added.
+	 */
 	public void addPropertyChangeListener(PropertyChangeListener pcl) {
 		support.addPropertyChangeListener(pcl);
 	}
 
+	/**
+	 * Removes a propertyChangeListener from the support variable.
+	 * @param pcl
+	 *        | the propertyChangeListener which will be removed
+	 */
 	public void removePropertyChangeListener(PropertyChangeListener pcl) {
 		support.removePropertyChangeListener(pcl);
 	}
 
+	/**
+	 * Returns all the components of the viewMode. (copy)
+	 */
 	public List<Component> getComponents() {
 		return new ArrayList<>(components);
 	}
 
+	/**
+	 * Sets the list of components to the viewMode.
+	 * 
+	 * @param components
+	 *        | A list of components.
+	 * @throws IllegalArgumentException when the components equal null
+	 *        | components == null
+	 * @post the list of components are equals to the components parameter
+	 *        | new.getComponents() == components
+	 */
 	protected void setComponents(List<Component> components) {
 		if (components == null) {
 			throw new IllegalArgumentException("The components of a view mode cannot be null");
@@ -44,6 +97,17 @@ public abstract class ViewMode implements PropertyChangeListener {
 		this.components = components;
 	}
 
+	/**
+	 * Adds a component to the list of components.
+	 * 
+	 * @param component
+	 *        | The component which will be added to the viewMode.
+	 * @return whether or not the component is added to the list of components.
+	 * @throws IllegalArgumentException when the component is equal to null.
+	 *       | component == null
+	 * @effect the component is added to the list of components.
+	 *       | components.add(component)
+	 */
 	public boolean addComponent(Component component) {
 		if (component == null) {
 			throw new IllegalArgumentException("Cannot add null as component to the view mode.");
@@ -51,24 +115,66 @@ public abstract class ViewMode implements PropertyChangeListener {
 		return components.add(component);
 	}
 
+	/**
+	 * Adds a list of components to the list of components.
+	 * 
+	 * @param c
+	 *       | The collection of components which needs to be added to the list of components.
+	 * @return whether or not the collection is added to the list of components.
+	 * @effect The collection of components is added to the list of components
+	 *       |components.addAll(c)
+	 */
 	public boolean addAllComponents(Collection<? extends Component> c) {
 		return components.addAll(c);
 	}
 
+	/**
+	 * Removes a component at a given index.
+	 * 
+	 * @param index
+	 *        | The index of which the component needs to be deleted.
+	 * @effect The component is removed at the given index.
+	 *        | components.remove(index)
+	 */
 	public void removeComponent(int index) {
 		components.remove(index);
 	}
 
+	/**
+	 * 
+	 * Removes a components from the list of components.
+	 * 
+	 * @param component
+	 *        | The component which needs to be deleted.
+	 * @effect The component is removed from the list.
+	 *        | components.remove(component)
+	 */
 	public void removeComponent(Component component) {
 		components.remove(component);
 	}
 
+	/**
+	 * Draws all the components of a viewMode.
+	 * @param g 
+	 * 		 This object offers the methods that allow you to paint on the canvas.
+	 */
 	public void paint(Graphics g) {
 		for (Component component : components) {
 			component.paint((Graphics2D) g.create());
 		}
 	}
 
+	/**
+	 * Adds a component to the list of ClickListeners.
+	 * If the component isn't already in the list.
+	 * @param c
+	 *        |The component which will be added to the clickListeners
+	 * @throws IllegalArgumentException when the component equals null
+	 *        | c == null
+	 * @effect the component is added to the list of ClickListeners
+	 *        |  if (!this.getClickListeners().contains(c))
+	 *        | 	this.clickListeners.add(c);
+	 */
 	protected void addClickListener(Component c) {
 		if (c == null)
 			throw new IllegalArgumentException("A new click listener cannot be null");
@@ -76,6 +182,17 @@ public abstract class ViewMode implements PropertyChangeListener {
 			this.clickListeners.add(c);
 	}
 
+	/**
+	 * Adds a component to the list of KeyListeners.
+	 * If the component isn't already in the list.
+	 * @param c
+	 *        |The component which will be added to the KeyListeners
+	 * @throws IllegalArgumentException when the component equals null
+	 *        | c == null
+	 * @effect the component is added to the list of KeyListeners
+	 *        |  if (!this.getKeyListeners().contains(c))
+	 *        | 	this.keyListeners.add(c);
+	 */
 	protected void addKeyListener(Component c) {
 		if (c == null)
 			throw new IllegalArgumentException("A new key listener cannot be null");
@@ -83,8 +200,21 @@ public abstract class ViewMode implements PropertyChangeListener {
 			this.keyListeners.add(c);
 	}
 
-	// We create a copy to make sure we're not editing the list while we are still
-	// looping through it
+	/**
+	 * Tells every component of the viewMode whether there was a click inside or outside the component.
+	 *  A copy is made to make sure there is no editing of the list while it is looping.
+	 * 
+	 * 
+	 * @param id
+	 *        | The id of the mouse event.
+	 * @param x
+	 *        | The x-coordinate of the component.
+	 * @param y
+	 *        | The y-coordinate of the component.
+	 * @param clickCount
+	 *        | The count of clicks.
+	 * @effect The components are called an told whether there was a click inside or outside.
+	 */
 	public void mouseClicked(int id, int x, int y, int clickCount) {
 		List<Component> currentClickListeners = new ArrayList<>(getClickListeners());
 		for (Component c : currentClickListeners) {
@@ -96,8 +226,18 @@ public abstract class ViewMode implements PropertyChangeListener {
 		}
 	}
 
-	// We create a copy to make sure we're not editing the list while we are still
-	// looping through it
+	/**
+	 * Delegates the keyPressed event to the components of the ViewMode.
+	 * A copy is made to make sure there is no editing of the list while it is looping.
+	 * 
+	 * @param id
+	 *        | The id of the key pressed event. 
+	 * @param keyCode
+	 *        | The key code of the key pressed event.
+	 * @param keyChar
+	 *        | The key character of a key pressed event.
+	 * @effect The components are called an told whether there was a keyPressed.
+	 */
 	public void keyPressed(int id, int keyCode, char keyChar) {
 		List<Component> currentKeyListeners = new ArrayList<>(getKeyListeners());
 		for (Component c : currentKeyListeners) {
@@ -105,14 +245,34 @@ public abstract class ViewMode implements PropertyChangeListener {
 		}
 	}
 
+	/**
+	 * Returns the viewModeType of the viewMode.
+	 */
 	public ViewModeType getViewModeType() {
 		return type;
 	}
 
+	/**
+	 * Returns whether or not the component already is in the list of components.
+	 * 
+	 * @param component
+	 *        | The component which will be checked if it is in the list of components.
+	 * @return true if the component occurs in the list of components; otherwise false.
+	 */
 	public boolean hasComponent(Component component) {
 		return this.getComponents().contains(component);
 	}
 
+	/**
+	 * Sets the type of the ViewMode.
+	 * 
+	 * @param type
+	 *        | the ViewModeType of the viewMode.
+	 * @throws IllegalArgumentException if the type equals null
+	 *        | type == null
+	 * @post the type of the viewMode is equal to the type parameter.
+	 *        | new.getType() == type
+	 */
 	protected void setType(ViewModeType type) {
 		if (type == null) {
 			throw new IllegalArgumentException("ViewModeType cannot be null in a viewmode.");
@@ -120,26 +280,45 @@ public abstract class ViewMode implements PropertyChangeListener {
 		this.type = type;
 	}
 
+	/**
+	 * This method fires a property change with the given propertyChangeEvent.
+	 * @param evt
+	 *        | The propertyChangeEvent used to fire the change.
+	 */
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		this.support.firePropertyChange(evt);
 	}
 
+	/**
+	 * Handles the throw error of a component with the given ID. 
+	 * @param id
+	 *        | The id of which element an error is thrown.
+	 */
 	public void throwError(UUID id) {
 		for (Component c : getComponents()) {
 			c.throwError(id);
 		}
 	}
 
+	/**
+	 * Removes all the clickListeners and KeyListeners from the viewMode.
+	 */
 	protected void removeAllClickAndKeyListeners() {
 		this.clickListeners.clear();
 		this.keyListeners.clear();
 	}
 
+	/**
+	 * Returns all the clickListeners of the viewMode.
+	 */
 	protected List<Component> getClickListeners() {
 		return this.clickListeners;
 	}
 
+	/**
+	 * Returns all the KeyListeners of the viewMode.
+	 */
 	protected List<Component> getKeyListeners() {
 		return this.keyListeners;
 	}
