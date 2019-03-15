6973e288511893766955f786ff4bc01a81e1ac44
Dries Janse <djse@live.be>
2019-03-15 17:02:06 +0100

    TablesViewMode Doc

--- src/ui/model/viewmodes/TablesViewMode.java
+++ src/ui/model/viewmodes/TablesViewMode.java
@@ -7,10 +7,28 @@ import ui.model.components.Container;
 import ui.model.components.TableList;
 import ui.model.components.TextField;
 
+/**
+ * A TablesViewMode is specific ViewMode.
+ *  It contains a container which stores all the names of tables.
+ * 
+ * @version 1.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel, Mauro Luyten
+ *
+ */
 public class TablesViewMode extends ViewMode {
 
+	/**
+	 * Variable storing the container.
+	 */
 	private Container container;
 
+	/**
+	 * Initialises a new TablesViewMode with the given information.
+	 * 
+	 * @param map
+	 *        | Containing all the UUID's and names of the tables.
+	 * @effect the full Tables list is created and all the information is set.
+	 */
 	public TablesViewMode(Map<UUID, String> map) {
 		super();
 		this.setType(ViewModeType.TABLESVIEWMODE);
@@ -18,6 +36,11 @@ public class TablesViewMode extends ViewMode {
 		this.addComponent(new TextField(50, 5, 200, 25, "TABLES LIST"));
 	}
 
+	/**
+	 * Creates a tableList out of the given information.
+	 * @param map
+	 *        | the map containing all the information of a viewMode.
+	 */
 	private void createTableList(Map<UUID, String> map) {
 		container = new Container(0, 0, 600, 600);
 		this.addComponent(getContainer());
@@ -31,12 +54,20 @@ public class TablesViewMode extends ViewMode {
 		getContainer().addComponent(tableList);
 	}
 
+	/**
+	 * Updates the old table and initialises it with all the new information.
+	 * @param map
+	 *        | the map containing all the information of a viewMode.
+	 */
 	public void updateTables(Map<UUID, String> map) {
 		this.removeAllClickAndKeyListeners();
 		this.removeComponent(getContainer());
 		this.createTableList(map);
 	}
 
+	/**
+	 * Returns the container variable of the TablesViewMode.
+	 */
 	private Container getContainer() {
 		return container;
 	}
