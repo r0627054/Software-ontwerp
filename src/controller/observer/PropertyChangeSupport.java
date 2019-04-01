8c42c6eb95478e87b0fca115c6496f0bc71b8b93
Dries Janse <djse@live.be>
2019-04-01 19:01:21 +0200

    propertyChangeSupport doc

--- src/controller/observer/PropertyChangeSupport.java
+++ src/controller/observer/PropertyChangeSupport.java
@@ -3,14 +3,37 @@ package controller.observer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The propertyChangeSupport used in the observer pattern.
+ *  It stores a list of listeners and can fire a property change to their listeners.
+ * @version 1.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel
+ *
+ */
 public class PropertyChangeSupport {
 
-	List<PropertyChangeListener> listeners;
+	/**
+	 * Variable storing the propertyChangeListeners.
+	 */
+	private List<PropertyChangeListener> listeners;
 	
+	/**
+	 * Initialises a new PropertyChangeSupport.
+	 *  The listeners is set to an empty ArrayList.
+	 */
 	public PropertyChangeSupport() {
 		listeners = new ArrayList<>();
 	}
 	
+	/**
+	 * Adds a propertyChangeListener to the list.
+	 * @param listener
+	 *        | The propertyChangeListener which will be added to the list.
+	 * @effect Adds a propertyChangeListener to the list.
+	 *        | listeners.add(listener);
+	 * @throws IllegalArgumentException when the listener equals null.
+	 *        | listener == null
+	 */
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		if(listener == null) {
 			throw new IllegalArgumentException("Cannot add a null listener");
@@ -18,6 +41,15 @@ public class PropertyChangeSupport {
 		this.getListeners().add(listener);
 	}
 	
+	/**
+	 * Removes a propertyChangeListener from list.
+	 * @param listener
+	 *        | The propertyChangeListener which will be removed to the list.
+	 * @effect Adds a propertyChangeListener to the list.
+	 *        | listeners.remove(listener);
+	 * @throws IllegalArgumentException when the listener equals null.
+	 *        | listener == null
+	 */
 	public void removePropertyChangeListener(PropertyChangeListener listener) {
 		if(listener == null) {
 			throw new IllegalArgumentException("Cannot remove a null listener");
@@ -25,16 +57,30 @@ public class PropertyChangeSupport {
 		this.getListeners().remove(listener);
 	}
 
+	/**
+	 * Returns the list of listeners.
+	 */
 	private List<PropertyChangeListener> getListeners() {
 		return listeners;
 	}
 	
+	/**
+	 * Notifies all the listeners that a property is changed.
+	 * It gives the event to all the listeners.
+	 * @param evt The propertyChangeEvent which will to all the listeners.
+	 * @post All the listeners are notified.
+	 *       | for(PropertyChangeListener listener: getListeners())
+	 *       |    listener.propertyChange(evt)
+	 */
 	public void firePropertyChange(PropertyChangeEvent evt) {
 		for(PropertyChangeListener listener: getListeners()) {
 			listener.propertyChange(evt);
 		}
 	}
 	
+	/**
+	 * Returns the size of the list of listeners.
+	 */
 	public int size() {
 		return this.getListeners().size();
 	}
