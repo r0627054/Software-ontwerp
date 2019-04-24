54c5ae569dae52d63c2a248ea92a04704124b607
Dries Janse <djse@live.be>
2019-04-24 17:18:06 +0200

    Deleting a table closes all the subwindows of the table.

--- src/controller/handlers/DeleteTableChangeHandler.java
+++ src/controller/handlers/DeleteTableChangeHandler.java
@@ -30,6 +30,7 @@ public class DeleteTableChangeHandler implements ChangeHandlerInterface {
 		try {
 			UUID tableId = evt.getSource();
 			domainfacade.deleteTable(tableId);
+			uifacade.closeAllSubWindowsOfTable(tableId);
 			uifacade.updateTablesSubWindows(domainfacade.getTableNames());
 		} catch (Exception e) {
 			e.printStackTrace();
