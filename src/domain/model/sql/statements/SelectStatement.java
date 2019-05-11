f04524ef1e9530e82ba30f0b09245983023d0017
Dries Janse <djse@live.be>
2019-05-11 16:08:08 +0200

    Basic SelectStatement

--- src/domain/model/sql/statements/SelectStatement.java
+++ src/domain/model/sql/statements/SelectStatement.java
@@ -4,8 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import domain.model.sql.CellId;
+import domain.model.sql.SqlException;
+import domain.model.sql.columnSpec.ColumnSpec;
 
 public class SelectStatement implements Statement {
+
+	private List<ColumnSpec> columnSpecs;
+	
+	public SelectStatement() {
+		this.columnSpecs = new ArrayList<>();
+	}
+	
+	
+	public void addColumnSpec(ColumnSpec columnSpec) {
+		if(columnSpec == null) {
+			throw new SqlException("The columnSpec cannot be null");
+		}
+		this.getColumnSpecs().add(columnSpec);
+	}
+	
+	
+	private List<ColumnSpec> getColumnSpecs() {
+		return columnSpecs;
+	}
+
+
 
 	@Override
 	public List<CellId> getAllCellIds() {
