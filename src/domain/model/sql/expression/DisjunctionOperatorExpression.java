f6936246f2dba57d75f5bc83de4923fdd4bf9e1c
Dries Janse <djse@live.be>
2019-05-19 21:09:52 +0200

    DisjunctionOperatorExpression Doc

--- src/domain/model/sql/expression/DisjunctionOperatorExpression.java
+++ src/domain/model/sql/expression/DisjunctionOperatorExpression.java
@@ -9,12 +9,31 @@ import domain.model.ValueType;
 import domain.model.sql.CellId;
 import domain.model.sql.Operator;
 
+/**
+ * A ConjunctionOperatorExpression is an OperatorExpression which has an "OR" operator.
+ * 
+ * @version 3.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel
+ *
+ */
 public class DisjunctionOperatorExpression extends OperatorExpression {
 
+	/**
+	 * An instance of a DisjunctionOperatorExpression is created with the given parameters.
+	 * @param leftExpression The left expression of the disjunction.
+	 * @param rightExpression The right expression of the disjunction.
+	 * @param operator       The operator which will be set.
+	 */
 	public DisjunctionOperatorExpression(Expression leftExpression, Expression rightExpression, Operator operator) {
 		super(leftExpression, rightExpression, operator);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * It simplifies the expression with the row and cellIdMap.
+	 * It can be simplified only when the left and right expression are cellId's or BooleanExpressions.
+	 *  This is made because of edge cases. This class will handle the disjunction edge cases and how it will be simplified.
+	 */
 	@Override
 	public Expression simplify(Row row, Map<CellId, Integer> cellIdMap) {
 		Expression left = getLeftExpression().simplify(row, cellIdMap);
@@ -48,13 +67,22 @@ public class DisjunctionOperatorExpression extends OperatorExpression {
 		return new BooleanExpression(false);
 	}
 	
+	/**
+	 * Extracts the cellId value out of the expression.
+	 * @param exp a cellIdExpression.
+	 * @param row The row on which the current calculation is in progress.
+	 * @param cellIdMap The mapping of cellId to the index.
+	 * @return The domainCell a the given index in the row.
+	 */
 	private DomainCell getDomainCellOfOutOfCellId(Expression exp, Row row, Map<CellId, Integer> cellIdMap) {
 		CellId cellId = ((CellIdExpression) exp).getValue();
 		Integer index = cellIdMap.get(cellId);
 		return row.getCellAtIndex(index);
 	}
 	
-
+	/**
+	 * Returns the disjunction operator expression is not editable.
+	 */
 	@Override
 	public Object[] isEditable() {
 		Object[] result = {new HashMap<CellId,Integer>(),false};
