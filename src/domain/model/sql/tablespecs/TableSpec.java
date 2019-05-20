9538da4c68fd9acaf7dfe9b17d52c96abd725206
Dries Janse <djse@live.be>
2019-05-20 10:28:39 +0200

    TableSpec Doc

--- src/domain/model/sql/tablespecs/TableSpec.java
+++ src/domain/model/sql/tablespecs/TableSpec.java
@@ -2,20 +2,41 @@ package domain.model.sql.tablespecs;
 
 import domain.model.sql.SqlException;
 
+/**
+ * A TableSpec is building block of the from statement.
+ *  It is an abstract class. Every TableSpec contains a realTableName and a display table name. (Display table name = table name locally used within the query)
+ * 
+ * @version 3.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel
+ *
+ */
 public abstract class TableSpec {
 
+	/**
+	 * Variable storing the real name of the table.
+	 */
 	private String realTableName;
+	
+	/**
+	 * Variable storing the display name of the table (= table name locally used within the query)
+	 */
 	private String displayTableName;
 
 	/**
-	 * @return the realTableName
+	 * Returns the real name of the table.
+	 * @return the realTableName The actual real name of the table.
 	 */
 	public String getRealTableName() {
 		return realTableName;
 	}
 
 	/**
-	 * @param realTableName the realTableName to set
+	 * Sets the real name of the table.
+	 * @param realTableName the realTableName which will be set.
+	 * @throws SqlException hen the realTableName equals null or when the name is empty.
+	 *         | realTableName == null || realTableName.trim().isEmpty()
+	 * @post the realTableName is set to the give parameter name.
+	 *         | new.getRealTableName() == realTableName 
 	 */
 	protected void setRealTableName(String realTableName) {
 		if (realTableName == null || realTableName.trim().isEmpty()) {
@@ -25,14 +46,20 @@ public abstract class TableSpec {
 	}
 
 	/**
-	 * @return the displayTableName
+	 * Returns the display name of the table.
+	 * @return the display table name of the table. (Display table name = table name locally used within the query)
 	 */
 	public String getDisplayTableName() {
 		return displayTableName;
 	}
 
 	/**
-	 * @param displayTableName the displayTableName to set
+	 * Sets the display name of the table.
+	 * @param displayTableName the display name of the table  (Display table name = table name locally used within the query)
+	 * @throws SqlException hen the displayTableName equals null or when the name is empty.
+	 *         | displayTableName == null || displayTableName.trim().isEmpty()
+	 * @post the displayTableName is set to the give parameter name.
+	 *         | new.getDisplayTableName() == displayTableName 
 	 */
 	protected void setDisplayTableName(String displayTableName) {
 		if (displayTableName == null || displayTableName.trim().isEmpty()) {
