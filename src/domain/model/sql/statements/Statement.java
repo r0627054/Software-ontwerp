08d118d29988888a7ada0a7dea1d40311dd1884d
Dries Janse <djse@live.be>
2019-05-20 09:45:24 +0200

    Statement doc

--- src/domain/model/sql/statements/Statement.java
+++ src/domain/model/sql/statements/Statement.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 import domain.model.sql.CellId;
 
+/**
+ * A statement is interface implemented by all the different SQL Statements.
+ * 
+ * @version 3.0
+ * @author Dries Janse, Steven Ghekiere, Laurens Druwel
+ *
+ */
 public interface Statement {
+	/**
+	 * Returns all the CellIds the specific statement uses.
+	 * @return a list of all the CellIds the specific statement uses.
+	 */
 	public List<CellId> getAllCellIds();
 }
